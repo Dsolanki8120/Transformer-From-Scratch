e2904b21b230f89a06f3faa802db0ad0283d042c
Deepak Solanki <solankideepak00652@gmail.com>
2025-10-07 00:02:16 -0700

    Updated Model.py and Transformer.ipynb with latest changes

--- Model.py
+++ Model.py
@@ -42,3 +42,34 @@ class positionalEncoding(nn.Module):
         return self.dropout(x)
 
 
+# layer Normalization.....
+
+class LayerNormalization(nn.Module):
+    def __init__(self,eps:float=10**-6)->None:
+        super().__init__()
+        self.eps= eps
+        self.alpha= nn.parameter(torch.ones(1))
+        self.bias= nn.parameter(torch.zeros(0))
+
+    def forward(self,x):
+        mean= x.mean(dim= -1,keepdim=True)
+        std= x.std(dim= -1,keepdim=True)
+        return self.alpha*(x-mean)/(std+self.eps) + self.bias
+
+
+# Feed Forword Neural Network class
+
+class FNN(nn.Module):
+    def __init__(self,d_model:int,d_ff:int,dropout:float)->None:
+        super().__init__()
+        self.linear_1= nn.Linear(d_model,d_ff)
+        self.dropout= nn.Dropout(dropout)
+        self.linear_2= nn.Linear(d_ff,d_model)
+
+    def forward(self,x):
+        # (Batch,seq-len,d_model)----> (Batch,seq-len,d_ff)----> (Batch,seq-len,d_model):
+
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+        
+
